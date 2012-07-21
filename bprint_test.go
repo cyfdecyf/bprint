f31d1b17f58d1cb1c27b3692187104b0da711a61
Chen Yufei <cyfdecyf@gmail.com>
2012-07-21 16:50:46 +0800

    Use switch statement in readData for better performance.
    See comment in bprint_test.go BenchmarkReadData for benchmark result.

--- bprint_test.go
+++ bprint_test.go
@@ -5,17 +5,24 @@ import (
 )
 
 func BenchmarkReadData(b *testing.B) {
+	// Benchmark setup:
+	//   Intel Q6600 CPU, Debian 6 with Go 1.0.1
+	//   11MB random binary data, with default binary format specifier
+	//
+	// Execution time:
+	//   use function         ~3.6s
+	//   use switch statement ~2.7s
 	b.StopTimer()
-	readFunc := parseBinaryFormatStr(defautlBinaryFmt)
-	readFuncLen := len(readFunc)
-	data := make([]interface{}, readFuncLen, readFuncLen)
+	formatDesc := parseBinaryFormatStr(defautlBinaryFmt)
+	formatDescLen := len(formatDesc)
+	data := make([]interface{}, formatDescLen, formatDescLen)
 
 	var err error
 	for i := 0; i < b.N; i++ {
 		bufReader, readCloser := openFile("testdata/bindata")
 		b.StartTimer()
 		for err == nil {
-			_, err = readData(bufReader, readFunc, data)
+			_, err = readData(bufReader, formatDesc, data)
 		}
 		b.StopTimer()
 		readCloser.Close()
